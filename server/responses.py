ac29c0e4183ebf9fdc1ea1e2907d182c3f6b33cc
Ramiro Detke <ramiro.fer.det@gmail.com>
2022-10-13 19:25:01 -0300

    response classes for 200 and 404

--- server/responses.py
+++ server/responses.py
@@ -1,10 +1,25 @@
 class Response:
-    pass
+    def __init__(self, code, content_type, body):
+        self.code = code
+        self.content_type = content_type
+        self.body = body
 
 
 class Response200(Response):
-    pass
+    def __init__(self, content_type, body):
+        super(Response200, self).__init__('200 OK', content_type, body)
 
 
 class Response404(Response):
-    pass
+    def __init__(self, content_type, body):
+        super(Response404, self).__init__('404 Not Found', content_type, body)
+
+
+class JsonResponse200(Response200):
+    def __init__(self, body):
+        super(JsonResponse200, self).__init__('application/json', body)
+
+
+class JsonResponse404(Response):
+    def __init__(self, body):
+        super(JsonResponse404, self).__init__('application/json', body)
