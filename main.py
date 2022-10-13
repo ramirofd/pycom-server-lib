28670e5f12331d9e09821e7833beef3eadbd8745
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-10-13 19:11:40 -0300

    Se agrego primer intento de conexion

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from server import Server
+import ujson
+from boards import Sensors
+from pycoproc_1 import Pycoproc # Para pysensev1
+# from pycoproc_2 import Pycoproc #Para pysensev2
 
+# Para pysensev2
+# py = Pycoproc()
+# if py.read_product_id() != Pycoproc.USB_PID_PYSENSE:
+#     raise Exception('Not a Pysense')
+# Fin para pysensev2
+py = Pycoproc(Pycoproc.PYSENSE) #Para pysensev1
+pySensor = Sensors(py)
 server = Server()
+
+# @server.get('/temperature')
+def get_temperature(json: str):
+    return ujson.dumps({"value" : pySensor.get_data("temperature")})
+
+print(type(get_temperature("temperature").encode()))
