f4aea9e01a901e59168215d5821de20d3b9a7e02
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-10-24 15:47:09 -0300

    Se elimino libreria pycoproc.py y pycoproc_2.py ya que con pycoproc_1.py se puede configurar ambas versiones de la pysense y la pyscan main: se agregaron las otras placas

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-# from lib.pycoproc_1 import Pycoproc # Para pysensev1
-from lib.pycoproc_2 import Pycoproc #Para pysensev2
-from boards.pysense1 import py_sense_api
+from lib.pycoproc_1 import Pycoproc
+# from boards.pysense1 import py_sense_api
+from boards.pytrack1 import py_track_api
+# from boards.scan1 import py_scan_api
 from server import WlanServer
 from server import Network
 
 import pycom
+
 pycom.heartbeat(False)
 
 # py = Pycoproc(Pycoproc.PYSENSE)
-py = Pycoproc()
-py_sense_api.set_pycoproc(py)
+py = Pycoproc(Pycoproc.PYTRACK)
+# py = Pycoproc(Pycoproc.PYSCAN)
+# py_sense_api.set_pycoproc(py)
+py_track_api.set_pycoproc(py)
+# py_scan_api.set_pycoproc(py)
 
-server = WlanServer(api=py_sense_api)
+# server = WlanServer(api=py_sense_api)
+server = WlanServer(api=py_track_api)
+# server = WlanServer(api=py_scan_api)
 nets = [
     Network(ssid='LCD', pwd='1cdunc0rd0ba'),
     Network(ssid='NoTengoWiFi', pwd='nohaywifi'),
