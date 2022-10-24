869707aaee54c7ab38a8248f445408e92db2fef0
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-10-24 15:42:14 -0300

    Se modifico el nombre de cada una de las funciones para que concuerden con su respectivo parametro y se pulio la recepcion del cambio de color

--- boards/pysense1.py
+++ boards/pysense1.py
@@ -1,5 +1,7 @@
+from time import time
 import ujson
 import pycom
+import time
 
 from server import REST
 from server import RestApi
@@ -7,8 +9,9 @@ from server import JsonResponse200
 from SI7006A20 import SI7006A20
 from LIS2HH12 import LIS2HH12
 from LTR329ALS01 import LTR329ALS01
-from MPL3115A2 import MPL3115A2, ALTITUDE, PRESSURE
+from MPL3115A2 import MPL3115A2, PRESSURE
 
+NO_COLOUR = 0x000000
 
 class PySense(RestApi):
 
@@ -33,42 +36,38 @@ class PySense(RestApi):
             "acceleration": LIS2HH12(pycoproc)
         }
 
-
 py_sense_api = PySense()
 
-
 @py_sense_api.rest.get('/light', 'Returns light value')
-def get_temperature(json: str):
+def get_light(json: str):
     resp = str(JsonResponse200(ujson.dumps({"value": py_sense_api.get_data("light")})))
     return resp
 
-
 @py_sense_api.rest.get('/humidity', 'Returns humidity value')
-def get_temperature(json: str):
+def get_humidity(json: str):
     resp = str(JsonResponse200(ujson.dumps({"value": py_sense_api.get_data("humidity")})))
     return resp
-
 
 @py_sense_api.rest.get('/temperature', 'Returns temperature value')
 def get_temperature(json: str):
     resp = str(JsonResponse200(ujson.dumps({"value": py_sense_api.get_data("temperature")})))
     return resp
 
-
 @py_sense_api.rest.get('/pressure', 'Returns pressure value')
-def get_temperature(json: str):
+def get_pressure(json: str):
     resp = str(JsonResponse200(ujson.dumps({"value": py_sense_api.get_data("pressure")})))
     return resp
 
-
 @py_sense_api.rest.get('/acceleration', 'Returns acceleration value')
-def get_temperature(json: str):
+def get_acceleration(json: str):
     resp = str(JsonResponse200(ujson.dumps({"value": py_sense_api.get_data("acceleration")})))
     return resp
 
-@py_sense_api.rest.post('/color', 'Change color led on the board', {'color': 'int - hexadecimal value of the RGB color'})
+@py_sense_api.rest.post('/color', 'Change color led on the board', {'color': 'str - hexadecimal value of the RGB color'})
 def post_change_color(json: str):
     d = ujson.loads(json)["color"]
-    pycom.rgbled(int(hex(d)))
+    pycom.rgbled(int((d)))
+    time.sleep(5)
+    pycom.rgbled(NO_COLOUR)
     resp = str(JsonResponse200(ujson.dumps({"value": "OK"})))
     return resp
